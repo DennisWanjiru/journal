dd03af4c23ead1a3f37e23413e2bcb5e976b066a
Dennis Wanjiru <denniswanjiru71@gmail.com>
2023-07-20 10:55:17 +0300

    add alert to improve user experience

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import { Timestamp, addDoc, collection } from "firebase/firestore";
-import { SyntheticEvent, useContext, useState } from "react";
+import { SyntheticEvent, useContext, useEffect, useState } from "react";
 
 import { JournalType } from "../types";
 import { AuthContext } from "../context/AuthContext";
 import { db } from "../config/firebase";
+import classNames from "classnames";
 
 export default function Home() {
   const navigate = useNavigate();
   const [content, setContent] = useState("");
   const [submitting, setSubmitting] = useState(false);
   const [error, setError] = useState("");
+  const [showSuccess, setShowSuccess] = useState(false);
 
   const { state, dispatch } = useContext(AuthContext);
   const { currentUser } = state;
+
+  useEffect(() => {
+    if (showSuccess) {
+      setTimeout(() => {
+        setShowSuccess(false);
+      }, 5000);
+    }
+  }, [showSuccess]);
 
   const onSubmit = async (e: SyntheticEvent) => {
     try {
@@ -34,6 +44,7 @@ export default function Home() {
 
       await addDoc(collection(db, "journals"), journal);
       setContent("");
+      setShowSuccess(true);
     } catch (err: any) {
       setError(err?.message ?? "Something went wrong!");
     } finally {
@@ -42,7 +53,7 @@ export default function Home() {
   };
   return (
     <>
-      <nav className="flex w-full justify-between bg-pink-500 items-center py-5 px-10 text-white">
+      <nav className="flex w-full relative justify-between bg-pink-500 items-center py-5 px-10 text-white">
         <h1 className="md:text-3xl text-xl font-semibold">Daybreak Journal</h1>
         <button
           onClick={() => {
@@ -80,6 +91,38 @@ export default function Home() {
           </button>
         </form>
       </main>
+
+      <div
+        className={classNames(
+          "bg-teal-100 border-l-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md absolute top-20 right-0 mt-2 opacity-0",
+          {
+            "opacity-100": showSuccess,
+          }
+        )}
+        role="alert"
+      >
+        <div className="flex items-center">
+          <div>
+            <p className="font-bold">Success: Your journal was sent.</p>
+            <p className="text-sm">
+              You will receive a journal tomorrow at 12PM from your fellow
+              journaler
+            </p>
+          </div>
+
+          <span className="ml-4 py-3" onClick={() => setShowSuccess(false)}>
+            <svg
+              className="fill-current h-6 w-6 text-teal-600"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+      </div>
     </>
   );
 }
