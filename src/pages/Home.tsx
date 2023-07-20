9f10e346dfc062d4bf44d06643c2416ce570eaff
Dennis Wanjiru <denniswanjiru71@gmail.com>
2023-07-20 03:10:37 +0300

    add signout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   const [submitting, setSubmitting] = useState(false);
   const [error, setError] = useState("");
 
-  const { state } = useContext(AuthContext);
+  const { state, dispatch } = useContext(AuthContext);
   const { currentUser } = state;
 
   const onSubmit = async (e: SyntheticEvent) => {
@@ -41,30 +41,44 @@ export default function Home() {
     }
   };
   return (
-    <div className="flex w-full h-screen items-center justify-center">
-      <form
-        className="flex w-1/3 justify-center flex-col bg-white p-20 gap-y-5"
-        onSubmit={onSubmit}
-      >
-        <h3 className="font-semibold text-2xl">Create journal</h3>
-
-        <div className="input-field">
-          <label>Content</label>
-          <textarea
-            placeholder="Write your journal here ..."
-            maxLength={500}
-            value={content}
-            className="input"
-            onChange={(e) => setContent(e.target.value)}
-          />
-        </div>
-
-        {error ? <span className="text-red-600">{error}</span> : null}
-
-        <button disabled={content.length < 10} className="btn" type="submit">
-          {submitting ? "Sharing..." : "Share"}
+    <>
+      <nav className="flex w-full justify-between bg-pink-500 items-center py-5 px-10 text-white">
+        <h1 className="text-3xl font-semibold">Daybreak Journal</h1>
+        <button
+          onClick={() => {
+            dispatch({ type: "SIGNOUT" });
+            navigate("/signin");
+          }}
+          className="px-5 py-2 border rounded-lg bg-transparent border-white text-white hover:bg-pink-600 hover:border-transparent"
+        >
+          Signout
         </button>
-      </form>
-    </div>
+      </nav>
+      <main className="flex w-full h-screen items-center justify-center">
+        <form
+          className="flex w-1/3 justify-center flex-col bg-white p-20 gap-y-5"
+          onSubmit={onSubmit}
+        >
+          <h3 className="font-semibold text-2xl">Create journal</h3>
+
+          <div className="input-field">
+            <label>Content</label>
+            <textarea
+              placeholder="Write your journal here ..."
+              maxLength={500}
+              value={content}
+              className="input"
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
+
+          {error ? <span className="text-red-600">{error}</span> : null}
+
+          <button disabled={content.length < 10} className="btn" type="submit">
+            {submitting ? "Sharing..." : "Share"}
+          </button>
+        </form>
+      </main>
+    </>
   );
 }
